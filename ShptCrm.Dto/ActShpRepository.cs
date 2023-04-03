eac72d171f3df6b99f241b910a0a705584810260
xasya <xasya89@mail.ru>
2023-04-03 20:30:57 +0300

    Изменение sql запроса отображающего списка действующих актов

--- ShptCrm.Dto/ActShpRepository.cs
+++ ShptCrm.Dto/ActShpRepository.cs
@@ -9,7 +9,7 @@ namespace ShptCrm.Dto
     public class ActShpRepository
     {
         public static async Task<IEnumerable<ActShpt>> GetList(MySqlConnection con) 
-            => await con.QueryAsync<ActShpt>("SELECT * FROM actshpt WHERE Status=1 OR Status=0 ORDER BY ActDate DESC LIMIT 100");
+            => await con.QueryAsync<ActShpt>("SELECT * FROM actshpt WHERE Status=1 ORDER BY ActDate DESC LIMIT 100");
 
         public static async Task<ActShpt> Get(MySqlConnection con, int actId)
             => await con.QuerySingleOrDefaultAsync<ActShpt>("SELECT * FROM actshpt WHERE id="+actId);
